d547d618ad8cfc0ce64dc9858c268f670f767c9f
mertcankose <mertcankose97@outlook.com>
2020-07-29 19:42:47 +0300

    first commit

---
+++ components/icons/Tickİcon.js
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+function SvgTickicon(props) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 7 5" fill="none" {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M6.806.182a.69.69 0 00-.933 0l-3.54 3.322-1.207-1.132a.69.69 0 00-.933 0 .593.593 0 000 .876l1.674 1.57a.69.69 0 00.933 0l4.006-3.76a.592.592 0 000-.876"
+        fill="#989898"
+        fillOpacity={0.3}
+      />
+    </svg>
+  );
+}
+
+export default SvgTickicon;
---
+++ stories/2-Icons.stories.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import * as Icons from "../components/icons";
+
+export default {
+  title: 'Icons',
+
+};
+
+export const Icon = () => (
+    <div className="icons">
+        <Icons.Tickİcon/>
+        <Icons.Tableİcon/>
+        <Icons.Shape/>
+        <Icons.Settingsİcon/>
+        <Icons.Sessionsİcon/>
+        <Icons.Searchİcon/>
+        <Icons.PaperPlaneİcon/>
+        <Icons.Notificationİcon/>
+        <Icons.Meetingsİcon/>
+        <Icons.Logo/>
+        <Icons.Fill2/>
+        <Icons.FilesBoardİcon/>
+        <Icons.Dashboardİcon/>
+        <Icons.Conversationsİcon/>
+        <Icons.Cardsİcon/>
+        <Icons.Calendarİcon/>
+    </div>
+);
